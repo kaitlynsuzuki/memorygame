6e66fe35c812ba1a6149a6a1a84865841c8ae270
Glitch (memorygame1) <none>
2021-03-25 03:21:17 +0000

    🌟🍛 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# memorygame
+# Pre-work - *Memory Game*
+
+**Memory Game** is a Light & Sound Memory game to apply for CodePath's SITE Program. 
+
+Submitted by: **Kaitlyn Suzuki**
+
+Time spent: **20** hours spent in total
+
+Link to project: https://memorygame1.glitch.me/
+
+## Required Functionality
+
+The following **required** functionality is complete:
+
+* [X] Game interface has a heading (h1 tag), a line of body text (p tag), and four buttons that match the demo app
+* [X] "Start" button toggles between "Start" and "Stop" when clicked. 
+* [X] Game buttons each light up and play a sound when clicked. 
+* [X] Computer plays back sequence of clues including sound and visual cue for each button
+* [X] Play progresses to the next turn (the user gets the next step in the pattern) after a correct guess. 
+* [X] User wins the game after guessing a complete pattern
+* [X] User loses the game after an incorrect guess
+
+The following **optional** features are implemented:
+
+* [X] Any HTML page elements (including game buttons) has been styled differently than in the tutorial
+* [X] Buttons use a pitch (frequency) other than the ones in the tutorial
+* [X] More than 4 functional game buttons
+* [ ] Playback speeds up on each turn
+* [X] Computer picks a different pattern each time the game is played
+* [X] Player only loses after 3 mistakes (instead of on the first mistake)
+* [ ] Game button appearance change goes beyond color (e.g. add an image)
+* [ ] Game button sound is more complex than a single tone (e.g. an audio file, a chord, a sequence of multiple tones)
+* [ ] User has a limited amount of time to enter their guess on each turn
+
+The following **additional** features are implemented:
+
+- [X] Dark mode and light mode implemented
+- [X] Three difficulty levels that differ in number of buttons and length of sound
+- [X] Piano version that mimics a piano in tone and plays back "Twinkle Twinkle Little Star"
+
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+![](https://i.imgur.com/M1dOHM1.gif)
+![](https://i.imgur.com/hWQcMPI.gif)
+
+## Reflection Questions
+1. If you used any outside resources to help complete your submission (websites, books, people, etc) list them here. 
+https://www.w3schools.com/js/js_switch.asp
+https://coolors.co/ ">https://coolors.co/
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+https://stackoverflow.com/questions/5898656/check-if-an-element-contains-a-class-in-javascript/5898748
+https://pages.mtu.edu/~suits/notefreqs.html
+
+2. What was a challenge you encountered in creating this submission (be specific)? How did you overcome it? (recommended 200 - 400 words) 
+
+  * The biggest challenge I experienced throughout this process was managing the multiple difficulty levels. In my implementation of the game, the user can choose between the easy, medium, and hard versions, with the more difficult versions including more buttons, less time to play the sound, and a longer pattern.  The aspect of this that was hardest for me to manage, was the different difficulty states that displayed differing amounts of buttons and allowing for the user to toggle between the different levels. I utilized two switch statements that when given a new difficulty level would change which buttons included the hidden class, to change the visibility of the buttons. I then needed to text the buttons controlling the difficult levels to make sure it was possible to switch between all three levels, while maintaining the correct layout. The most difficult part was controlling the logic within the switch statement so I did not end up removing the hidden class from an element that did not currently have it.
+
+
+3. What questions about web development do you have after completing your submission? (recommended 100 - 300 words) 
+
+  * During this process, I was most surprised by the fact that this website only required one page, so I am very interested in learning how to create more complex websites that include multiple pages. Because of the experience I have with object oriented programming, javascript was relatively easy to pick up, so I am curious to explore the language deeper and compare the experience to python or java. I would also like to learn how to implement more complex functionality within elements of a webpage using javascript, as I know it is quite powerful. I also found the AudioContext library to be interesting and I would love to explore more javascript libraries to then build more complex web apps. Because this project was solely front end, I would like to know more about what exactly the back end does and then understand how the two work together as a full stack application.
+
+4. If you had a few more hours to work on this project, what would you spend them doing (for example: refactoring certain functions, adding additional features, etc). Be specific. (recommended 100 - 300 words) 
+
+  * If given the chance I would spend extra time making the website more aesthetically pleasing. I have an interest in informatics and design, but I do not have the CSS skills to turn my ideas into a reality yet. The general design is somewhat bland and almost dated, so I would love to improve the look of the buttons and their positioning. In addition, I would like to turn the darkmode button into a switch, which I did a little research on, but ultimately opted out. This would adhere to the industry standard of darkmode switches and would be interesting as it includes the “input” tag, which I only know a little about. I unfortunately did not get the chance to include the timed feature, which only allowed users to select the buttons within a specific time frame, so I would also try to explore and include this. While I am satisfied with the functionally of my code, there are perhaps optimizations I could have made to make my code more clean, efficient, or understandable, so I would try to read through my code and try to note where improvements could be made.
+
+
+
+## License
+
+    Copyright [Kaitlyn Suzuki]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ script.js
@@ -0,0 +1,294 @@
+// global constants
+var clueHoldTime = 1000; //how long to hold each clue's light/sound
+
+var cluePauseTime = 333; //how long to pause in between clues
+var nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+
+//Global Variables
+var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var progress = 0; 
+var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5;  //must be between 0.0 and 1.0
+var guessCounter = 0;
+var darkmode = false;
+var difficulty = 0;
+var piano = 0;
+var map;
+var prefix = "button";
+
+
+function startGame(){
+    //initialize game variables
+    progress = 0;
+    gamePlaying = true;
+    // swap the Start and Stop buttons
+    document.getElementById("startBtn").classList.add("hidden");
+    document.getElementById("stopBtn").classList.remove("hidden");
+    context.resume();
+    pattern = createPattern();
+
+    playClueSequence();
+}
+
+function stopGame(){
+    gamePlaying = false;
+    document.getElementById("stopBtn").classList.add("hidden");
+    document.getElementById("startBtn").classList.remove("hidden");
+}
+
+// Sound Synthesis Functions
+const freqMap = {
+  1: 261.6,
+  2: 293.66,
+  3: 329.63,
+  4: 369.99,
+  5: 392.00,
+  6: 440.00
+}
+const pianoMap = {
+  1: 261.6,
+  2: 277.18,
+  3: 293.66,
+  4: 311.13,
+  5: 329.63,
+  6: 349.23,
+  7: 369.99,
+  8: 392.00,
+  9: 415.30,
+  10: 440.00,
+  11: 466.16,
+  12: 493.88,
+  13: 523.25
+}
+function playTone(btn,len){
+  if (piano == 1){
+    map = pianoMap;
+  }
+  else{
+    map = freqMap;
+  }
+  o.frequency.value = map[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if (piano == 1){
+    map = pianoMap;
+  }
+  else{
+    map = freqMap;
+  }
+  if(!tonePlaying){
+    o.frequency.value = map[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    tonePlaying = true
+  }
+}
+function stopTone(){
+    g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+    tonePlaying = false
+}
+
+//Page Initialization
+// Init Sound Synthesizer
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
+
+function lightButton(btn){
+  if (piano == 0){
+    document.getElementById(prefix+btn).classList.add("hidden");
+    document.getElementById("img"+btn).classList.remove("hidden");
+  }
+  else{
+    document.getElementById(prefix+btn).classList.add("lit");
+  }
+
+}
+function clearButton(btn){
+  if (piano == 0){
+    document.getElementById(prefix+btn).classList.remove("hidden");
+    document.getElementById("img"+btn).classList.add("hidden");
+  }
+  else{
+    document.getElementById(prefix+btn).classList.remove("lit");
+  }
+}
+function playSingleClue(btn){
+  if(gamePlaying){
+    lightButton(btn);
+    playTone(btn,clueHoldTime);
+    setTimeout(clearButton,clueHoldTime,btn);
+  }
+}
+function playClueSequence(){
+  guessCounter = 0;
+  let delay = nextClueWaitTime; //set delay to initial wait time
+  for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
+    console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
+    setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
+    delay += clueHoldTime 
+    delay += cluePauseTime;
+  }
+}
+function loseGame(){
+  stopGame();
+  alert("Game Over. You lost.");
+}
+function winGame(){
+  stopGame();
+  alert("Game Over. You win!");
+}
+function guess(btn){
+  console.log("user guessed: " + btn);
+  if(!gamePlaying){
+    return;
+  }
+  if (btn == pattern[guessCounter]){
+    if (guessCounter == progress){
+      if (progress == pattern.length - 1){
+        winGame()
+      }
+      else{
+        progress++;
+        playClueSequence();
+      }
+    }
+    else{
+      guessCounter++;
+    }
+  }
+  else{
+    loseGame();
+  }
+
+  // add game logic here
+}
+function changeMode()
+{
+  if (!darkmode){
+    //if currently lightmode it will be switched to darkmode
+    changeMode1("darkmode", "lightmode");
+    darkmode = true;
+  }
+  else{
+    //if currently darkmode it will be switched to lightmode
+    changeMode1("lightmode", "darkmode");
+    darkmode = false;
+  }
+}
+function changeMode1(mode1, mode2)
+{
+  document.getElementById("body").classList.add(mode1);
+  document.getElementById("body").classList.remove(mode2);
+  let buttonList = document.getElementsByTagName("button");
+  for(let i = 0; i < buttonList.length; i++){
+    buttonList[i].classList.add(mode2);
+    buttonList[i].classList.remove(mode1);
+  }
+}
+
+function setSelected(val)
+{
+  if (!gamePlaying && val != difficulty){
+    switch (difficulty){
+    case 0:
+      document.getElementById("easyBtn").classList.remove("selected");
+      break;
+    case 1:
+      document.getElementById("mediumBtn").classList.remove("selected");
+      document.getElementById("button5").classList.add("hidden");
+      break;
+    case 2:
+      document.getElementById("hardBtn").classList.remove("selected");
+      document.getElementById("button5").classList.add("hidden");
+      document.getElementById("button6").classList.add("hidden");
+      break;
+    case 3:
+      document.getElementById("pianoBtn").classList.remove("selected");
+      document.getElementById("pianoButtonArea").classList.add("hidden");
+      document.getElementById("gameButtonArea").classList.remove("hidden");
+      prefix = "button";
+      piano = 0;
+      break;
+    }
+    switch(val){
+    case 0:
+      document.getElementById("easyBtn").classList.add("selected");
+      document.getElementById("button5").classList.add("hidden");
+      if (!document.getElementById("button6").classList.contains("hidden")){
+        document.getElementById("button5").classList.add("hidden");
+      }
+      clueHoldTime = 1000; //how long to hold each clue's light/sound
+
+      cluePauseTime = 333; //how long to pause in between clues
+      nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+      break;
+    case 1:
+      document.getElementById("mediumBtn").classList.add("selected");
+      document.getElementById("button5").classList.remove("hidden");
+      clueHoldTime = 500;
+
+      cluePauseTime = 200;
+      nextClueWaitTime = 500;
+      break;
+    case 2:
+      document.getElementById("hardBtn").classList.add("selected");
+      document.getElementById("button5").classList.remove("hidden");
+      document.getElementById("button6").classList.remove("hidden");
+      clueHoldTime = 250;
+
+      cluePauseTime = 100;
+      nextClueWaitTime = 250;
+      break;
+    case 3:
+      document.getElementById("pianoBtn").classList.add("selected");
+      document.getElementById("gameButtonArea").classList.add("hidden");
+      document.getElementById("pianoButtonArea").classList.remove("hidden");
+      piano = 1;
+      prefix = "pianoButton";
+      clueHoldTime = 500;
+
+      cluePauseTime = 200;
+      nextClueWaitTime = 500;
+      break;
+    }
+    difficulty = val;
+  }
+}
+function createPattern(){
+  var len;
+  var numOfButtons;
+  if (difficulty == 3){
+    return [1, 1, 8, 8, 10, 10, 8, 6, 6, 5, 5, 3, 3, 1];
+  }
+  switch (difficulty){
+    case 0:
+      len = 5;
+      numOfButtons = 4;
+      break;
+    case 1:
+      len = 9;
+      numOfButtons = 5;
+      break;
+    case 2:
+      len = 14;
+      numOfButtons = 6;
+      break;
+    }
+  var e = [];
+  for(let i = 0; i < len; i++){
+    let j = Math.floor(Math.random() * numOfButtons) + 1;
+    e.push(j);
+  }
+  console.log(e);
+  return e;
+}
